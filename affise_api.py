6b50e60ce6348da8c7876cc781c113f2aa78988b
mlaistseva <mlaistseva@emphasoft.com>
2020-07-31 00:49:25 +0300

    fix bugs

--- affise_api.py
+++ affise_api.py
@@ -1,25 +1,29 @@
 import requests
 
+URL = 'http://api.cpanomer1.affise.com/3.0/'
+HEADER = {
+    "API-Key": 'e60a98867d363b0d43b9e7c58ec498ed'
+}
 
-def affise_api():
-    key = 'e60a98867d363b0d43b9e7c58ec498ed'
-    url = 'http://api.cpanomer1.affise.com/3.0/'
 
-    responce = requests.get(f'{url}partner/offers?api-key={key}')
-
-    offer = responce.json()['offers'][0]
+def get_offer():
+    response = requests.get(f'{URL}partner/offers', headers=HEADER)
+    offer = response.json()['offers'][0]
     print("Information about offer: ", offer)
     print("Available countries: ", offer['countries'])
+    return offer['id']
 
-    id = offer['id']
 
-    responce = requests.get(f"{url}stats/conversions?api-key={key}&status=")
-    conversion = responce.json()['conversions'][0]
+def get_conversion(off_id):
+    response = requests.get(f"{URL}stats/conversions?date_from=", headers=HEADER)
+    conversion = response.json()['conversions'][0]
     print("Information about conversion: ", conversion)
-    if id == conversion['offer_id']:
+    if off_id == conversion['offer_id']:
         print("Conversion click id: ", conversion['action_id'])
         print("Conversion from: ", conversion['country_name'], conversion['city'])
 
 
-affise_api()
+if __name__ == '__main__':
+    offer_id = get_offer()
+    get_conversion(offer_id)
 
